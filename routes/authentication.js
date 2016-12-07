33bb8dc62527b8b94c6e5bc5c291bea72f8e839f
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-12-06 18:44:02 -0600

    initialized sign in

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,4 +4,32 @@ const jwt = require('jwt-simple');
 const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 
+router.post('/signup', function(req, res, next) {
+  const { username, password } = req.body;
+
+  if(!username || !password) {
+    return res.status(422)
+    .json({ error: 'You must provide username and password' });
+  }
+
+  User.findOne({ username }).exec()
+  .then((existingUser) => {
+    if(existingUser) {
+      return res.status(422).json({ Error: 'Username is in use' });
+  }
+
+  bcrypt.hash(password, 10, function(err, hashedPassword) {
+    if(err) {
+      return next(err);
+    }
+
+    const user = new User({ username, password: hashedPassword });
+
+    user.save()
+      .then(user => res.json(user));
+    });
+  })
+  .catch(err => next(err));
+});
+
 module.exports = router;
--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,27 @@ const passport = require('passport');
 const User = require('../models/UserModel');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local');
+
+const signinStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }).exec()
+    .then(user => {
+      if(!user) {
+        return done (null, false);
+      }
+
+    bcrypt.compare(password, user.password, function(err, isMatch){
+      if (err) {
+        return done (null, false);
+      }
+
+      if (!isMatch) {
+        return done (null, false);
+      }
+
+      return done (null, user);
+    });
+  })
+  .catch(err => done(err, false));
+});
+
+passport.use(`signinStrategy`, signinStrategy);
